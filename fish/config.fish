b962bd6a43e14495028068bffc959c5d5a9833ee
oneils <lex.nox@gmail.com>
2025-05-10 01:06:53 +0200

    Added configs for Fish

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Setting PATH for Python 3.11
+# The original version is saved in /Users/lex/.config/fish/config.fish.pysave
+set -x PATH "/Library/Frameworks/Python.framework/Versions/3.11/bin" "$PATH"
+
+set -gx EDITOR nvim
+
+alias vvim vim
+alias vim nvim
+alias v nvim
+
--- install/install.sh
+++ install/install.sh
@@ -6,13 +6,14 @@ DOTFILES="$HOME/projects/dotfiles"
 NVIM_BACKUP="$HOME/.config/nvim.bkp"
 NVIM_CFG="$HOME/.config/nvim"
 
+FISH_BACKUP="$HOME/.config/fish.bkp"
+FISH_CFG="$HOME/.config/fish"
+
 # Create symbolic links for your configs
 echo "Creating symlinks..."
 
 # Neovim
-
 echo "Setting up Neovim config..."
-
 
 
 # remove existing bkp folder
@@ -36,7 +37,36 @@ mkdir -p "$NVIM_CFG"
 ln -sf "$DOTFILES/nvim/"* "$NVIM_CFG"/
 echo -e "\tsimlinks for Neovim created"
 
-# MacOS specific 
+
+# ==== Fish ====
+echo "Setting up Fish config..."
+
+
+# remove existing bkp folder
+ if [ -d "$FISH_BACKUP" ]; then
+   echo -e "\tRemoving existing backup directory"
+   rm -rf "$FISH_BACKUP" || {
+     echo -e "\tError: Failed to remove existing backup at $FISH_BACKUP"
+     exit 1
+   }
+ fi
+
+# backup existing configs folder
+if [ -d "$FISH_CFG" ]; then
+  echo -e "\tDoing backup of existing Fish config directory"
+  mv -f "$FISH_CFG" "$FISH_BACKUP" 
+  echo -e "\tExisting Fish configs folder moved to $FISH_BACKUP"
+fi
+
+echo -e "\tSetting up Fish config..."
+mkdir -p "$FISH_CFG"
+ln -sf "$DOTFILES/fish/"* "$FISH_CFG"/
+echo -e "\tsimlinks for Fish created"
+
+
+
+
+# ==== MacOS specific ====
 uname=$(uname)
 if [[ "$uname" == "Darwin" ]]; then
   . "$DOTFILES/install/macos.sh"
