4e219ac7fde173f2d925606bc96f663aa544f92c
oneils <lex.nox@gmail.com>
2025-05-11 23:13:57 +0200

    Added highlighting for nvim yanking

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,3 +38,13 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
