6556f7a6574b640dc7170dd0a087528381d15756
oneils <lex.nox@gmail.com>
2025-05-16 20:33:13 +0200

    Chanded some apps to floating window
    changes bg color for active windows to orange for Aerospace

--- nvim-lazyvim/lua/config/keymaps.lua
+++ nvim-lazyvim/lua/config/keymaps.lua
@@ -2,4 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = vim.keymap.set
-map("i", "jk", "<ESC>")
+map("i", "jj", "<ESC>")
