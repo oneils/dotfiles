047ab4339cb184d28b2b260c04cdcee8bcffac12
alexeibo <alexeybo@playtika.com>
2025-09-19 13:54:52 +0200

    Added ripgrep to brew formulas and enhanced Mason.nvim configuration with dependencies and automatic LSP installation.

--- nvim-lazyvim/lua/plugins/init.lua
+++ nvim-lazyvim/lua/plugins/init.lua
@@ -1,6 +1,10 @@
 return {
   {
     "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+    },
     opts = {
       ensure_installed = {
         "stylua",
@@ -14,6 +18,29 @@ return {
         "css-lsp",
         "prettier",
       },
+    },
+    config = function(_, opts)
+      require("mason").setup(opts)
+      local mr = require("mason-registry")
+      mr:on("package:install:success", function()
+        vim.defer_fn(function()
+          -- trigger FileType event to possibly load this newly installed LSP server
+          require("lazy.core.handler.event").trigger({
+            event = "FileType",
+            buf = vim.api.nvim_get_current_buf(),
+          })
+        end, 100)
+      end)
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    opts = {
+      automatic_installation = true,
     },
   },
   {
