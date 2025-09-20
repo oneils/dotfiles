dba4372b7849f7b2d4a949bb2648a06dab819184
oneils <lex.nox@gmail.com>
2025-09-21 00:04:50 +0200

    Added hot keys fot markdown headers

--- nvim-lazyvim/lua/config/keymaps.lua
+++ nvim-lazyvim/lua/config/keymaps.lua
@@ -3,3 +3,17 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 map("i", "jj", "<ESC>")
+
+-- Markdown headers (only in markdown files)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    -- Markdown header mappings with leader+m
+    map("n", "<leader>m1", "I# <Esc>", { desc = "Insert H1 header", buffer = true })
+    map("n", "<leader>m2", "I## <Esc>", { desc = "Insert H2 header", buffer = true })
+    map("n", "<leader>m3", "I### <Esc>", { desc = "Insert H3 header", buffer = true })
+    map("n", "<leader>m4", "I#### <Esc>", { desc = "Insert H4 header", buffer = true })
+    map("n", "<leader>m5", "I##### <Esc>", { desc = "Insert H5 header", buffer = true })
+    map("n", "<leader>m6", "I###### <Esc>", { desc = "Insert H6 header", buffer = true })
+  end,
+})
