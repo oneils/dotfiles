0a49ccd610520a194f82e52631a5a92daa651c95
oneils <lex.nox@gmail.com>
2025-11-18 22:08:18 +0100

    Minor changes

--- nvim-lazyvim/lua/plugins/colorshceme.lua
+++ nvim-lazyvim/lua/plugins/colorshceme.lua
@@ -1,13 +1,11 @@
 return {
-  -- { "ellisonleao/gruvbox.nvim" },
-
   { "ellisonleao/gruvbox.nvim" },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "folke/tokyonight.nvim" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      --       colorscheme = "gruvbox",
       colorscheme = "catppuccin",
     },
   },
