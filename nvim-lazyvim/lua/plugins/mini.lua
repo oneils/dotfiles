4128f360c7cc7661cfe7d6094dee073d2c97238a
oneils <lex.nox@gmail.com>
2026-02-24 14:08:03 +0100

    Added missed comment for the surround plugin
    -- Add/delete/replace surroundings (brackets, quotes, etc.)
    --
    -- - saiw)  - [S]urround [A]dd [I]nner [W]ord [)]Paren
    -- - sd'    - [S]urround [D]elete [']quotes
    -- - sr)'   - [S]urround [R]eplace [)] [']

--- nvim-lazyvim/lua/plugins/mini.lua
+++ nvim-lazyvim/lua/plugins/mini.lua
@@ -1,6 +1,11 @@
 return {
   'nvim-mini/mini.nvim',
   config = function()
+    -- Add/delete/replace surroundings (brackets, quotes, etc.)
+    --
+    -- - saiw)  - [S]urround [A]dd [I]nner [W]ord [)]Paren
+    -- - sd'    - [S]urround [D]elete [']quotes
+    -- - sr)'   - [S]urround [R]eplace [)] [']
     require('mini.surround').setup({
       mappings = {
         add = 'gza',
