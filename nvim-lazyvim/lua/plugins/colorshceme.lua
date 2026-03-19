02dfd4aecbd43c44bd679ed0f244cd1faddb20fd
oneils <lex.nox@gmail.com>
2026-03-19 20:59:05 +0100

    Added rose pine color scheme

--- nvim-lazyvim/lua/plugins/colorshceme.lua
+++ nvim-lazyvim/lua/plugins/colorshceme.lua
@@ -1,13 +1,22 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
+  { "shaunsingh/nord.nvim" },
+  { "rebelot/kanagawa.nvim" },
   { "catppuccin/nvim",         name = "catppuccin", priority = 1000 },
   { "folke/tokyonight.nvim" },
-  { "rose-pine/neovim",        name = "rose-pine",  opts = { variant = "moon" } },
+  { "rose-pine/neovim",        name = "rose-pine",  opts = { variant = "moon", styles = { italic = false } } },
+  { "rebelot/kanagawa.nvim" },
+  { "sainnhe/gruvbox-material" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      -- colorscheme = "kanagawa",
+      -- colorscheme = "gruvbox",
+      colorscheme = "rose-pine",
+      -- colorscheme = "gruvbox-material"
+      -- colorscheme = "kanagawa"
+      -- colorscheme = "catppuccin",
     },
   },
 }
